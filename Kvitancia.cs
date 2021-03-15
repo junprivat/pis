003c819551e5882a7a75177965e2d5875d847800
YoRHa431 <vladugorenko@gmail.com>
2021-03-15 12:03:37 +0300

    second

---
+++ Kvitancia.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Kvitancia
+    {
+        private int Number;
+        private string Date;
+        private float Symma;
+        public void SetNumber(int Number)
+        {
+            this.Number = Number;
+        }
+        public void SetDate(string Date)
+        {
+            this.Date = Date;
+        }
+        public void SetSymma(float Symma)
+        {
+            this.Symma = Symma;
+        }
+        public int GetNumber()
+        {
+            return this.Number;
+        }
+        public string GetDate()
+        {
+            return this.Date;
+        }
+        public float GetSymma()
+        {
+            return this.Symma;
+        }
+        public Kvitancia(int Number, string Date, float Symma)
+        {
+            this.Number = Number;
+            this.Date = Date;
+            this.Symma = Symma;
+        }
+        public Kvitancia()
+        { }
+    }
+}
